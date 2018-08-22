d18762398e5974a48101e0579cb9d66db0cf8e38
EleosOS <24414799+EleosOS@users.noreply.github.com>
2018-08-22 20:57:45 +0200

    Added gitignore

--- config.ts
+++ config.ts
@@ -1,8 +1,8 @@
 // Config is a ts file because TypeScript dislikes json files.
 
 export const config = {
-    token: '',
     authorization: '',
+    token: '',
     upvoterChannel: '417085568367722498',
     upvoterRole: '417407477974433802',
 };
