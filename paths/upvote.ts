a2e9ccd6708a8820657ac1be43c3179aa7638417
EleosOS <24414799+EleosOS@users.noreply.github.com>
2018-08-22 22:52:28 +0200

    breaks and fixes

--- paths/upvote.ts
+++ paths/upvote.ts
@@ -14,49 +14,49 @@ class UpvotePathClass {
     }
 
     private upvote(req: Request, res: Response) {
-        if (req.headers.authorization !== config.authorization) {
-            return signale.fatal('A request to /upvote arrived with the wrong authorization header...')
-        }
+        /*if (config.authorization.length > 0 && req.headers.authorization !== config.authorization) {
+            return signale.fatal('A request to /upvote arrived with the wrong authorization header...\n' + req.headers.authorization)
+        }*/
 
-        signale.start({prefix: '[upvote]', message: `Upvote recieved! Searching for ${req.body.user}...`, suffix: req.body.user});
+        signale.start({prefix: '[upvote]', message: `Upvote recieved! Searching for ${req.body.user}...`});
 
         const ease = bot.guilds.get('365236789855649814');
         const upvoter: Member | undefined = ease!.members.find((u) => u.id === req.body.user);
 
-        if (typeof upvoter === undefined) {
-            signale.pause({prefix: '[upvote]', message: `${upvoter.username} doesn't seem to be on the server...`, suffix: req.body.user});
-            signale.complete({prefix: '[upvote]', message: 'Sending message to #upvote-army.', suffix: req.body.user});
-            return bot.createMessage(config.upvoterChannel, `${upvoter.username} upvoted on DBL! But they aren't on the server to recieve their perks...`);
+        if (!upvoter) {
+            signale.pause({prefix: '[upvote]', message: `${req.body.user} doesn't seem to be on the server...`});
+            signale.complete({prefix: '[upvote]', message: 'Sending message to #upvote-army.'});
+            return bot.createMessage(config.upvoterChannel, `Someone upvoted on DBL! But they aren't on the server to recieve their perks...`);
         } else {
-            signale.pending({prefix: '[upvote]', message: `${upvoter.username} is on the server...`, suffix: req.body.user});
-            signale.pending({prefix: '[upvote]', message: `Adding role to ${upvoter.username}...`, suffix: req.body.user});
+            signale.pending({prefix: '[upvote]', message: `${upvoter.username} is on the server...`});
+            signale.pending({prefix: '[upvote]', message: `Adding role to ${upvoter.username}...`});
             upvoter.addRole(config.upvoterRole, 'Upvote on DBL');
             let msg: string;
 
             if (req.body.isWeekend) {
-                msg = `${getSuperb()}, <@${req.body.user}> has upvoted on DBL during an active voting multiplier!`;
+                msg = `${getSuperb()}, <@${upvoter.id}> has upvoted on DBL during an active voting multiplier!`;
             } else {
-                msg = `${getSuperb()}, <@${req.body.user}> has upvoted on DBL!`;
+                msg = `${getSuperb()}, <@${upvoter.id}> has upvoted on DBL!`;
             }
 
             this.setReminder(upvoter);
 
-            signale.complete({prefix: '[upvote]', message: 'Sending message to #upvote-army.', suffix: req.body.user});
+            signale.complete({prefix: '[upvote]', message: 'Sending message to #upvote-army.'});
             return bot.createMessage(config.upvoterChannel, msg);
         }
     }
 
     private setReminder(upvoter: Member): void {
-        signale.await({prefix: '[upvote]', message: `Setting a reminder for ${upvoter.username}... See you in 12 hours!`, suffix: upvoter.id});
+        signale.await({prefix: '[upvote]', message: `Setting a reminder for ${upvoter.username}... See you in 12 hours!`});
 
         setTimeout(async () => {
             try {
                 const channel = await upvoter.user.getDMChannel();
 
                 channel.createMessage('Hello! You can upvote Ease again.');
-                return signale.success({prefix: '[upvote]', message: `Sent a DM to ${upvoter.username}.`, suffix: upvoter.id});
+                return signale.success({prefix: '[upvote]', message: `Sent a DM to ${upvoter.username}.`});
             } catch (e) {
-                signale.error({prefix: '[upvote]', message: `${upvoter.username} probably has their DMs disabled.`, suffix: upvoter.id});
+                signale.error({prefix: '[upvote]', message: `${upvoter.username} probably has their DMs disabled.`});
                 signale.error(e);
             }
         }, 43200000);
--- server.ts
+++ server.ts
@@ -5,11 +5,16 @@ import { UpvotePath } from './paths/index';
 
 const app = express();
 const port: number | string = process.env.PORT || 3000;
+const shitArt: string = `(   )\n(   ) (\n) _   )\n( \\_\n_(_\\ \\)__\n(____\\___))`;
 
 app.use(bodyParser.json());
 
 app.use('/upvote', UpvotePath);
 
+app.get('/', (req, res) => {
+    res.send(shitArt);
+});
+
 const listener = app.listen(port, () => {
-    signale.start(`Listening at ${listener.address().address}`);
+    signale.start(`Listening at ${listener.address().address} PORT: ${port}`);
 });
