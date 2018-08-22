09fe4c153bd77d76acec3203ad20dad95441e1cb
Eleos <24414799+EleosOS@users.noreply.github.com>
2018-08-22 21:55:57 +0200

    Delete config.ts

--- config.ts
+++
@@ -1,8 +0,0 @@
-// Config is a ts file because TypeScript dislikes json files.
-
-export const config = {
-    authorization: '',
-    token: '',
-    upvoterChannel: '417085568367722498',
-    upvoterRole: '417407477974433802',
-};
