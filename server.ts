4beee98344328a23c25ae65e019dd2391fb05bcd
Eleos <24414799+EleosOS@users.noreply.github.com>
2018-10-27 22:04:03 +0200

    changed a 0 to a 1

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ import mongoose from 'mongoose';
 import { UpvotePath, ErrorPath, ErrorCache } from './paths/index';
 
 const app = express();
-const port: number = 3000;
+const port: number = 3001;
 const moustacheInfo: string = `<img src="https://cdn.discordapp.com/avatars/417105627253309450/f470851f6fbff7cd1739b3a327ff9f46.png?size=256" alt="moustache logo"><br /><p>moustacheOS ver 1.0.2</p>`;
 
 app.use(bodyParser.json());
