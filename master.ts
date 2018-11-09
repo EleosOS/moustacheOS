74f7015f8813b52b7293e1a24a7ca633ac890c90
Eleos <24414799+EleosOS@users.noreply.github.com>
2018-11-09 19:00:33 +0100

    Tweaked PR#1

--- master.ts
+++ master.ts
@@ -4,15 +4,12 @@ const port: number = 3001;
 
 console.log('--------------------\nStarting moustache\n--------------------');
 
-
-mongoose.connect('mongodb://localhost:27017/moustacheDB', { 
+mongoose.connect('mongodb://localhost:27017/moustacheDB', {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    autoReconnect: true, 
+    autoReconnect: true,
 })
     .then(() => console.log('[master] Connected to MongoDB.'))
-    .catch(console.log)
-
+    .catch(console.log);
 
 app.listen(port, () => {
     console.log(`[express] Listening at port ${port}`);
