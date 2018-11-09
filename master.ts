611c57e2c2b30511c2476a0e807295c57914416a
Khaazz <khaaz.dev@gmail.com>
2018-11-09 13:31:52 +0100

    Fixing indentation

--- master.ts
+++ master.ts
@@ -5,13 +5,13 @@ const port: number = 3001;
 console.log('--------------------\nStarting moustache\n--------------------');
 
 
-    mongoose.connect('mongodb://localhost:27017/moustacheDB', { 
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        autoReconnect: true, 
-    })
-        .then(() => console.log('[master] Connected to MongoDB.'))
-        .catch(console.log)
+mongoose.connect('mongodb://localhost:27017/moustacheDB', { 
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    autoReconnect: true, 
+})
+    .then(() => console.log('[master] Connected to MongoDB.'))
+    .catch(console.log)
 
 
 app.listen(port, () => {
