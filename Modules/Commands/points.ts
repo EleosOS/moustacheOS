aa1e0e7f5ad777917b90c7dea875ff590417f0b0
Eleos <24414799+EleosOS@users.noreply.github.com>
2018-12-26 21:26:19 +0100

    Made pointsChange a subcommand of points

--- Modules/Commands/points.ts
+++ Modules/Commands/points.ts
@@ -1,6 +1,40 @@
 import { MoustacheCommand } from './';
-import { bot } from '../';
+import { bot, Transactions } from '../';
 import { PointsModel } from '../../other/';
+
+const subPointsChange: MoustacheCommand = {
+    execute: async (msg, args) => {
+        const [ userID, amount ] = args;
+        let userPoints;
+
+        if (!userID || !amount) {
+            return 'Not enough arguments.';
+        } else if (!+amount) {
+            return 'Amount is not a number.';
+        }
+
+        if (+amount < 0) {
+            userPoints = await Transactions.substract(userID, +amount, 'Changed by a ruler');
+        } else {
+            userPoints = await Transactions.add(userID, +amount, 'Changed by a ruler');
+        }
+
+        if (!userPoints) {
+            return 'This action would result in negative points and has been aborted.'
+        }
+
+        return `<@${userID}>'s points are now at ${userPoints}.`;
+    },
+    label: 'pointsChange',
+    options: {
+        description: '*Ruler only* Changes points by given amount.',
+        fullDescription: '*Ruler only*\nIncrements or decrements the points of the given user by given amount.',
+        usage: '`userID` `amount`',
+        requirements: {
+            roleIDs: ['378293035852890124'],
+        }
+    }
+}
 
 export const points: MoustacheCommand = {
     execute: async (msg, args) => {
@@ -22,5 +56,6 @@ export const points: MoustacheCommand = {
         description: 'Shows how many points a user currently has.',
         fullDescription: 'Shows how many points a user currently has.',
         usage: '`userID`'
-    }
+    },
+    subcommands: [subPointsChange]
 }
