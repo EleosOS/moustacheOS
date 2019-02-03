332d8fe3298036f77e20277f4e364900a417217f
Eleos <24414799+EleosOS@users.noreply.github.com>
2019-02-03 20:50:44 +0100

    WIP Inventory

--- Modules/Inventory.ts
+++ Modules/Inventory.ts
@@ -1,10 +1,34 @@
+import { InventoryModel } from '../other/index';
+
 class InventoryClass {
 
+    createInv(userID: string, itemID: number) {
+        
+    }
+
+    getInv(userID: string) {
+
+    }
+
+    addItem(userID: string, itemID: number) {
+
+    }
+
+    removeItem(userID: string, itemID: number) {
+
+    }
+
+    useItem(userID: string, itemID: number) {
+
+    }
 }
+
+export const Inventory = new InventoryClass();
 
 /*
 Stuff:
 get inventory of a user
+create inventory
 add an item to a user
 remove an item
 use an item
--- Modules/Items/functionFoo.ts
+++ Modules/Items/functionFoo.ts
@@ -5,6 +5,6 @@ export const FunctionFoo: MoustacheItem = {
     type: 'function',
     price: 50,
     execute: async (msg) => {
-        msg.channel.createMessage(`hi this is a message of the past\nsince i know that you need 50 points to open this message, you just wasted 50 points\nrip <@${msg.author.id}>`)
+        msg.channel.createMessage(`hi this is a message from the past\nsince i know that you need 50 points to open this message, you just wasted 50 points\nrip <@${msg.author.id}>`)
     }
 }
